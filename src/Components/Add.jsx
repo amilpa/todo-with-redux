80fe5eb123d84f440cf8e04f9cf2e4b8f03715e6
Amil P A <amilpa2020@gmail.com>
2023-03-30 17:40:25 +0530

    added add

--- src/Components/Add.jsx
+++ src/Components/Add.jsx
@@ -1,9 +1,27 @@
 
-import React from 'react'
+import React, { useState } from 'react'
+import { addTodo } from '../redux/todoSlice'
+
+import { useDispatch } from 'react-redux'
 
 const Add = () => {
+
+	const [input , setInput] = useState('') 
+
+	const dispatch = useDispatch()
+
+	const handleClick = (event) => {
+		event.preventDefault()
+		dispatch(addTodo({text : input}))
+	}
+
 	return (
-		<div>Add</div>
+		<div>
+			<form>
+				<input type="text" value={input} onChange={(event) => setInput(event.target.value)}/>
+				<button onClick={handleClick}>Submit</button>
+			</form>
+		</div>
 	)
 }
 
